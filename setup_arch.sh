aa23db3bc70dd85afd2065249dd588130ae3eaef
The Infinity Glitch (Antonio Alan) <theinfinityglitch@gmail.com>
2025-11-01 07:38:30 -0300

    Fixes

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,11 +1,12 @@
-sudo pacman -S git clang krita inkscape steam spotify-launcher cmake firefox discord fish github-cli
+sudo pacman -Syu
+sudo pacman -S git clang krita inkscape steam spotify-launcher cmake firefox discord fish github-cli evtest
 sudo chsh -s /bin/fish
 fish -c 'fish_config prompt choose arrow'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -f https://zed.dev/install.sh | sh
 fish -c 'fish_add_path -U ~/.local/bin'
 sudo cp disable_keyboard.sh /usr/local/sbin/
-sudo cp diable_keyboard.service /etc/systemd/system/
+sudo cp disable_keyboard.service /etc/systemd/system/
 mkdir ~/Documents/coding
 mkdir ~/Documents/coding/rust
 mkdir ~/Documents/gamedev
@@ -15,5 +16,6 @@ cd lib_input_helper
 ~/.cargo/bin/cargo build --release
 sudo cp target/release/lib_input_helper /usr/bin/
 sudo systemctl daemon-reload
-sudo systemctl enable diable_keyboard.service
-fish
+sudo systemctl enable disable_keyboard.service
+reboot
+
